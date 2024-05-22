bb923386464cf2860b7d76981db6ac813717f406
wkif <kif101001000@163.com>
2024-05-22 17:02:02 +0800

    feat: 去除MulterModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import * as path from 'path';
+// import { MulterModule } from '@nestjs/platform-express';
+// import { diskStorage } from 'multer';
+// import * as path from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -29,19 +29,19 @@ console.log('oss', oss);
       load: [customConfig],
     }),
     TypeOrmModule.forRoot(database),
-    MulterModule.register({
-      storage: diskStorage({
-        // 指定文件存储目录
-        destination: path.join(__dirname, '../uploads'),
-        // 通过时间戳来重命名上传的文件名
-        filename: (_, file, callback) => {
-          const fileName = `${
-            new Date().getTime() + path.extname(file.originalname)
-          }`;
-          return callback(null, fileName);
-        },
-      }),
-    }),
+    // MulterModule.register({
+    //   storage: diskStorage({
+    //     // 指定文件存储目录
+    //     destination: path.join(__dirname, '../uploads'),
+    //     // 通过时间戳来重命名上传的文件名
+    //     filename: (_, file, callback) => {
+    //       const fileName = `${
+    //         new Date().getTime() + path.extname(file.originalname)
+    //       }`;
+    //       return callback(null, fileName);
+    //     },
+    //   }),
+    // }),
     UserModule,
     AuthModule,
     LinksModule,
--- src/config/dev.ts
+++ src/config/dev.ts
@@ -1,9 +1,5 @@
-import * as path from 'path';
-
 export default () => ({
   port: 3000,
-  uploadsPath: path.resolve(__dirname, '../../uploads/template'),
-  cachePath: path.resolve(__dirname, '../../cache'),
   database: {
     type: 'mysql',
     host: 'localhost',
